ec0d99a65984427f558666da49e615ee86230cc1
philip andrew <philip368320@gmail.com>
2021-06-01 13:44:35 +1000

    Update

--- copybinary.bat
+++ copybinary.bat
@@ -1,2 +1,3 @@
 del /Q .\camelnodejs\binary\camel-quarkus-runner.exe
+mkdir .\camelnodejs\binary
 copy .\camel-quarkus-binary\build\code-with-quarkus-1.0.0-SNAPSHOT-runner.exe .\camelnodejs\binary\camel-quarkus-runner.exe
